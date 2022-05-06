40b9ea1cf3f4c76abd2f743a4d5e18094662b594
okunlade Jolade <jokunlade@gmail.com>
2022-05-06 23:41:48 +0100

    Adding data meanings

--- src/App.js
+++ src/App.js
@@ -1,24 +1,32 @@
-import logo from './logo.svg';
+import axios from 'axios';
 import './App.css';
+import {useEffect, useState} from "react";
 
 function App() {
+
+const [meanings, setMeanings] = useState([]);
+const dictonaryApi = async() => {
+  try {
+    const data = await axios.get('https://api.dictionaryapi.dev/api/v2/entries/en/plane');
+  
+  
+  
+  setMeanings(data.data);
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+console.log(meanings);
+
+
+useEffect(() => {
+  dictonaryApi();
+}, [])
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <div className="App">Dictionary</div>
   );
 }
 
