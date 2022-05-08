64901d8a48c11b2080c745c2d249d42284612f3e
okunlade Jolade <jokunlade@gmail.com>
2022-05-08 13:38:34 +0100

    implemented dark and light mode

--- src/App.js
+++ src/App.js
@@ -1,7 +1,8 @@
 import axios from 'axios';
 import './App.css';
-import {useEffect, useState} from "react";
-import { Container } from '@mui/material';
+import {useEffect, useState, useSyncExternalStore} from "react";
+import { alpha, Container, styled, Switch } from '@mui/material';
+import { pink } from '@mui/material/colors';
 import Header from './components/Header/Header';
 import Definitions from './components/Header/Definitions/Definitions';
 
@@ -9,6 +10,19 @@ function App() {
 const [word, setWord] = useState("");
 const [meanings, setMeanings] = useState([]);
 const [category, setCategory] = useState("en");
+const [LightMode, setLightMode] = useState(false)
+
+const DarkMode = styled(Switch)(({ theme }) => ({
+  '& .MuiSwitch-switchBase.Mui-checked': {
+    color: pink[600],
+    '&:hover': {
+      backgroundColor: alpha(pink[600], theme.palette.action.hoverOpacity),
+    },
+  },
+  '& .MuiSwitch-switchBase.Mui-checked + .MuiSwitch-track': {
+    backgroundColor: pink[600],
+  },
+}));
 
 const dictonaryApi = async() => {
   try {
@@ -34,9 +48,22 @@ useEffect(() => {
 
   return (
     <div className="App" style={{height: "100vh",backgroundColor: "skyblue", color: 'white'}}>
-      <Container maxWidth="md" 
-      style={{display: "flex", flexDirection: "column", height: "100vh"}}>
+      <Container 
+      maxWidth="md" 
+      style={{display: "flex", 
+      flexDirection: "column", 
+      height: "100vh", 
+      justifyContent: 'space-evenly'}}>
       
+      <div 
+      style={{position: "absolute", top: 0, right: 15, paddingTop: 10}}>
+      
+      <span>{LightMode ? "Dark" : "Light"} Mode</span>
+      <DarkMode 
+      checked={LightMode} 
+      onChange={()=>setLightMode(!LightMode)} />
+      </div>
+
         <Header 
         category={category}
         setCategory={setCategory} 
